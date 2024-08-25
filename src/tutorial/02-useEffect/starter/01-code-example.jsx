a5dab4dca38519f26c6f0818ab92340d6c16c421
Mohit <Mohitmishra975@gmail.com>
2024-08-25 13:39:29 +0700

    Learning

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const sayHello = () => {
+    console.log('Hello There')
+  }
+
+  sayHello()
+
+  useEffect(() => {
+    console.log('Hello from use effect')
+  }, [])
 
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
-  );
-};
-export default CodeExample;
+  )
+}
+export default CodeExample
