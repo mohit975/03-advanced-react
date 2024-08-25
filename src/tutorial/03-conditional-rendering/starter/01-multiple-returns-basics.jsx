d7ef7c9e8df7eae798b020c1e9757fd3b42f4885
Mohit <Mohitmishra975@gmail.com>
2024-08-25 22:50:35 +0700

    learning react

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
-};
-export default MultipleReturnsBasics;
+  const [isloading, setISloading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setISloading(false)
+    }, 3000)
+  }, [])
+  if (isloading) {
+    return <h2>IsLoading ... </h2>
+  }
+  return <h2>Multiple Returns Basics</h2>
+}
+export default MultipleReturnsBasics
